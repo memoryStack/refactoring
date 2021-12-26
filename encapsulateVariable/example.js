51b2d7076fae3537c26763f1e64b77385625d8d3
Anuj Rao <anuj.rao@myntra.com>
2021-12-26 17:48:41 +0530

    encapsulate variable commit_3

--- encapsulateVariable/example.js
+++ encapsulateVariable/example.js
@@ -1,5 +1,4 @@
-const { getDefaultOwner, setDefaultOwner }  = require('./owner.js')
-// import  from "../encapsulateRecord/owner";
+const { getDefaultOwner, setDefaultOwner } = require('./owner.js')
 
 // after refactoring the code looks like below
 
@@ -9,9 +8,13 @@ console.log(spaceship.owner)
 setDefaultOwner({ firstName: "Sun", lastName: "Sunrise" })
 console.log(getDefaultOwner()) // set variable
 
-spaceship.owner.firstName = 42 // but we can still set the variables like this which we don't want
+spaceship.owner.firstName = 42 // but we can still set the variables like this with no valdations which we don't want
 console.log(spaceship.owner)
 
 /**
-    now we can put validations in the funcs where we are setting "defaultOwner" variable
+    now we can put validations in the funcs where we are setting "defaultOwner" variable.
+    now we can expose the functions which will let the updates happen to the object in a controlled way.
+    this is working one level deep only right now, if we want to control the access to the deeper properties
+    then we have to put more funcs in the class
+
 */
--- encapsulateVariable/owner.js
+++ encapsulateVariable/owner.js
@@ -1,7 +1,20 @@
 
 let defaultOwner = {firstName: "Martin", lastName: "Fowler"} // shared data through out the code
-function getDefaultOwner() {return defaultOwner} // function to read variable
+function getDefaultOwner() { return new Person(defaultOwner) } // function to read variable
 function setDefaultOwner(arg) {defaultOwner = arg} // function to write variable
+
+class Person {
+    constructor(data) {
+        this._firstName = data.firstName
+        this._lastName = data.lastName
+    }
+
+    get firstName() { return this._firstName }
+    set firstName(firstName) { this._firstName = firstName }
+
+    get lastName() { return this._lastName }
+    set lastName(lastName) { this._lastName = lastName }
+}
 
 module.exports = {
     getDefaultOwner,
