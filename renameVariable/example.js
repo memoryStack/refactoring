8b6fc866ac6717aae5d0a5307d579705e7198c3c
Anuj Rao <anuj.rao@myntra.com>
2021-12-30 16:31:38 +0530

    1. added rename variable commit_1 2. didn't cover all use-cases. come back to this technique again

---
+++ renameVariable/example.js
@@ -0,0 +1,25 @@
+// before refactoring the variable is like this
+const t = "some title"
+
+// after refactoring we want it to be something like this
+const title = "some title"
+
+/**
+ * here right now i am demonstrating only 1 use case
+ *      when variable is constant or read only
+ */
+
+const newName = "old value"
+const oldName = newName
+
+/**
+ * using this approach we can replace "oldName" with "newName" slowly slowly over multiple iterations.
+ * and once the replacement is done 100% then we can simply delete/comment statement "const oldName = newName"
+ * and after removing it if there are some problems occur then we can again uncommet the statement without much hassle.
+ * 
+ * Note: from personal experience
+ *      i was doing it like below earlier
+ *          const oldName = "old value"
+            const newName = oldName
+        but above approach is a little bit better. Think why ?
+ */
